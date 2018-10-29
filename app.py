db4e55127bae3fcec98b2105638a34958d6a7f2d
Kye Kim <ghkim@stanford.edu>
2018-10-29 12:06:22 -0700

    alexa handler ver 1

---
+++ app.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+# This is a simple Hello World Alexa Skill, built using
+# the implementation of handler classes approach in skill builder.
+import logging
+import pymysql
+import rds_config
+import sys
+from datetime import datetime, timedelta
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+sb = SkillBuilder()
+
+rds_host  = "cs206.ci8sromxgv9w.us-west-2.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+#fh = logging.FileHandler('spam.log')
+ch = logging.StreamHandler()
+logger.addHandler(ch)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+logger.info("connection worked")
+# speech_text = "not fetched"
+# last_hour_date_time = datetime.now() - timedelta(hours = 1)
+#
+# with conn.cursor() as cur:
+#     cur.execute('select * from articles where updated_at >= %s ORDER BY RAND() limit 1', last_hour_date_time.strftime('%Y-%m-%d %H:%M:%S'));
+#     result = cur.fetchall()
+#     for row in result:
+#       speech_text = "Today on New York Times, one homepage headline is %s. I peeked the first sentence, and it goes like this. %s" % (row[2], row[3])
+#       print (speech_text)
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+class WhatsNewIntentHandler(AbstractRequestHandler):
+    """Handler for Whats New Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("WhatsNewIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "I feel lazy... nothing is new."
+        last_hour_date_time = datetime.utcnow() - timedelta(hours = 1)
+
+        with conn.cursor() as cur:
+            cur.execute('select * from articles  ORDER BY RAND() limit 1');
+            result = cur.fetchall()
+            for row in result:
+              speech_text = "Today on New York Times, one homepage headline is %s. I peeked the first sentence, and it goes like this. %s" % (row[2], row[3])
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("News Loop", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("HelloWorldIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Hello Python World from Classes!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "You can say hello to me!"
+
+        handler_input.response_builder.speak(speech_text).ask(
+            speech_text).set_card(SimpleCard(
+                "Hello World", speech_text))
+        return handler_input.response_builder.response
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Goodbye!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+        return handler_input.response_builder.response
+
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = (
+            "The Hello World skill can't help you with that.  "
+            "You can say hello!!")
+        reprompt = "You can say hello!!"
+        handler_input.response_builder.speak(speech_text).ask(reprompt)
+        return handler_input.response_builder.response
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        return handler_input.response_builder.response
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Catch all exception handler, log exception and
+    respond with custom message.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speech = "Sorry, there was some problem. Please try again!!"
+        handler_input.response_builder.speak(speech).ask(speech)
+
+        return handler_input.response_builder.response
+
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(WhatsNewIntentHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+handler = sb.lambda_handler()
---
+++ rds_config.py
@@ -0,0 +1,4 @@
+#config file containing credentials for rds mysql instance
+db_username = "cs206master"
+db_password = "Getthenews!"
+db_name = "articles"
